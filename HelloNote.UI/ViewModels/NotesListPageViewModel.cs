ed3066590e850cb7ff5475d868efb798b5f4d21e
Jesse Freeman <jessefreeman@gmail.com>
2024-06-21 10:38:17 -0700

    Lesson 2.1 - Creating the ViewModel.

---
+++ HelloNote.UI/ViewModels/NotesListPageViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using HelloNote.Shared;
+
+namespace HelloNote.UI.ViewModels
+{
+	public class NotesListPageViewModel : BindableObject
+	{
+
+		private ObservableCollection<Note> _notes;
+
+		public ObservableCollection<Note> Notes
+		{
+			get => _notes;
+			set
+			{
+				_notes = value;
+				OnPropertyChanged();
+			}
+		}
+
+		public NotesListPageViewModel()
+		{
+			Notes = new ObservableCollection<Note>();
+			RefreshNotes();
+		}
+
+        private void RefreshNotes()
+        {
+			Notes.Clear();
+			Notes.Add(new Note { Title = "Sample Note", Content = "This is a sample note" });
+            Notes.Add(new Note { Title = "Another Note", Content = "This is another sample note" });
+        }
+    }
+}
+
