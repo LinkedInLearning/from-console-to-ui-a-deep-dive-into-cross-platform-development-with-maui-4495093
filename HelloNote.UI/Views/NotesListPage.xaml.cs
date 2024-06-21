70d2a808ae6c80949c6d38fc7bfb93c76c88f85e
Jesse Freeman <jessefreeman@gmail.com>
2024-06-21 11:25:43 -0700

    Lesson 2.2 - Creating the View for the Notes List

---
+++ HelloNote.UI/Views/NotesListPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using HelloNote.UI.ViewModels;
+
+namespace HelloNote.UI.Views;
+
+public partial class NotesListPage : ContentPage
+{
+	public NotesListPage()
+	{
+		InitializeComponent();
+		BindingContext = new NotesListPageViewModel();
+	}
+
+}
